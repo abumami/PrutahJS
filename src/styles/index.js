423487c2bd3d11e71c72e3ccd95286c74f4f290a
abumami <abumami@users.noreply.github.com>
2016-08-23 13:33:34 +0300

    Add files via upload

---
+++ src/styles/index.js
@@ -0,0 +1,13 @@
+var styles = {
+    transparentBg: {
+        background: 'transparent'
+    },
+    center: {
+        margin: 'auto',
+        width: '50%',
+        padding: '10px'
+    }
+
+}
+
+module.exports = styles;
